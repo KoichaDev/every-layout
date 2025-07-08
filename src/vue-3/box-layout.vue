83cba832d7ce352cb366cf6d9d5afff91e2e8329
Khoi Hoang <khoii87@gmail.com>
2025-07-08 17:28:54 +0200

    Update box-layout.vue

--- src/vue-3/box-layout.vue
+++ src/vue-3/box-layout.vue
@@ -6,11 +6,23 @@ const {
   is = "div",
   padding = "1em",
   borderWidth = 0,
+  paddingBlock,
+  paddingInline,
+  paddingTop,
+  paddingRight,
+  paddingBottom,
+  paddingLeft,
   invert,
 } = defineProps<{
   is?: keyof NativeElements;
   padding?: CSSProperties["padding"];
+  paddingBlock?: CSSProperties["paddingBlock"];
+  paddingInline?: CSSProperties["paddingInline"];
   borderWidth?: CSSProperties["borderWidth"];
+  paddingTop?: CSSProperties["paddingTop"];
+  paddingRight?: CSSProperties["paddingRight"];
+  paddingLeft?: CSSProperties["paddingLeft"];
+  paddingBottom?: CSSProperties["paddingBottom"];
   invert?: boolean;
 }>();
 
@@ -23,9 +35,15 @@ defineExpose({ nativeElementExposeRef: nativeElementRef });
   <component
     ref="native-element-ref"
     :is="is"
-    class="box-l"
+    class="box-layout"
     :class="{
       invert: invert ?? 'invert',
+      'box-padding-block': paddingBlock,
+      'box-padding-inline': paddingInline,
+      'box-padding-top': paddingTop,
+      'box-padding-right': paddingRight,
+      'box-padding-bottom': paddingBottom,
+      'box-padding-left': paddingLeft,
     }"
     v-bind="$attrs"
   >
@@ -34,10 +52,42 @@ defineExpose({ nativeElementExposeRef: nativeElementRef });
 </template>
 
 <style scoped>
-.box-l {
+.box-layout {
   padding: v-bind(padding);
   border: v-bind(borderWidth) solid;
   color: var(--neutrals-foreground-primary, #0d0d0e);
+}
+
+.box-padding-top {
+  padding: 0;
+  padding-top: v-bind(paddingTop);
+}
+
+.box-padding-right {
+  padding: 0;
+  padding-right: v-bind(paddingRight);
+}
+
+.box-padding-left {
+  padding: 0;
+  padding-left: v-bind(paddingLeft);
+}
+
+.box-padding-bottom {
+  padding: 0;
+  padding-bottom: v-bind(paddingBottom);
+}
+
+.box-padding-block {
+  padding: 0;
+  padding-inline: v-bind(paddingInline);
+  padding-block: v-bind(paddingBlock);
+}
+
+.box-padding-inline {
+  padding: 0;
+  padding-block: v-bind(paddingBlock);
+  padding-inline: v-bind(paddingInline);
 }
 
 .invert {
